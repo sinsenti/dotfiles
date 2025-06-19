19af19e212d354c8a6f147ccc96c8399864f19ec
sinsenti <sinsenti73@gmail.com>
2025-06-19 10:33:40 +0300

    Auto-push: 2025-06-19 10:33:40

--- .config/scripts/rofi_combi.sh
+++ .config/scripts/rofi_combi.sh
@@ -4,18 +4,24 @@ DIR1="$HOME/dotfiles/.config/scripts"
 DIR2="$HOME/.config/hypr/UserScripts"
 DIR3="$HOME/.config/hypr/scripts"
 
-echo -e "calculator\ntranslator\nemoji\nshutdown\nsuspend\nsearch\nwallpaper\n" |
+echo -e "calculator\ntranslator\nemoji\nshutdown\nsuspend\nsearch\nwallpaper\ndownloads\ngit" |
   rofi -i -dmenu -p "Select:" |
   while read -r choice; do
     case "$choice" in
-    calculator)
-      bash "$DIR2/RofiCalc.sh"
-      ;;
     translator)
       bash $DIR1/rofi_translate.sh
       ;;
+    calculator)
+      bash "$DIR2/RofiCalc.sh"
+      ;;
+    wallpaper)
+      bash $DIR2/WallpaperSelect.sh
+      ;;
     emoji)
       bash $DIR3/RofiEmoji.sh
+      ;;
+    search)
+      bash $DIR3/RofiSearch.sh
       ;;
     shutdown)
       shutdown now
@@ -23,11 +29,11 @@ echo -e "calculator\ntranslator\nemoji\nshutdown\nsuspend\nsearch\nwallpaper\n" 
     suspend)
       systemctl suspend
       ;;
-    search)
-      bash $DIR3/RofiSearch.sh
+    downloads)
+      thunar Downloads
       ;;
-    wallpaper)
-      bash $DIR2/WallpaperSelect.sh
+    git)
+      thunar git
       ;;
     *)
       # If no valid choice, just exit
