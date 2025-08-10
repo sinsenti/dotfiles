77c30368ca4bc090847841e2739d0ff310ce272b
sinsenti <sinsenti73@gmail.com>
2025-08-10 23:33:24 +0300

    Auto-push: 2025-08-10 23:33:24

--- .config/scripts/rofi_translate.sh
+++ .config/scripts/rofi_translate.sh
@@ -43,4 +43,53 @@ TRANSLATION=$(trans -b ":$TARGET_LANG" "$TEXT")
 CHOICE=$(echo -e "Copy\nClose" | rofi -mesg "$TRANSLATION" -dmenu -p "Done:")
 if [ "$CHOICE" = "Copy" ]; then
   echo -n "$TRANSLATION" | wl-copy
+elif [ "$CHOICE" = "Close" ]; then
+  exit 0
+else
+  while true; do
+    INPUT=$CHOICE
+    echo "Printing input: $INPUT"
+    if [ $? -ne 0 ]; then
+      exit 0
+    fi
+    DIRECTION=$(echo "$INPUT" | cut -d' ' -f1)
+    TEXT=$(echo "$INPUT" | cut -d' ' -f2-)
+
+    # List of valid directions
+    if [[ "$DIRECTION" == "en,ru" ]] || [[ "$DIRECTION" == "ru,en" ]] ||
+      [[ "$DIRECTION" == "ru" ]] || [[ "$DIRECTION" == "кг" ]] ||
+      [[ "$DIRECTION" == "en,es" ]] || [[ "$DIRECTION" == "ru,es" ]] ||
+      [[ "$DIRECTION" == "es,en" ]]; then
+
+      # Set languages based on direction
+      if [ "$DIRECTION" = "en,ru" ]; then
+        TARGET_LANG="ru"
+      elif [ "$DIRECTION" = "ru,en" ] || [ "$DIRECTION" = "ru" ] || [ "$DIRECTION" = "кг" ]; then
+        TARGET_LANG="en"
+      elif [ "$DIRECTION" = "en,es" ]; then
+        TARGET_LANG="es"
+      elif [ "$DIRECTION" = "ru,es" ]; then
+        TARGET_LANG="es"
+      elif [ "$DIRECTION" = "es,en" ]; then
+        TARGET_LANG="en"
+      fi
+
+    else
+      TARGET_LANG="ru"
+      TEXT="$INPUT"
+    fi
+    if [ -z "$CHOICE" ]; then
+      exit 0
+    fi
+
+    TRANSLATION=$(trans -b ":$TARGET_LANG" "$TEXT")
+    CHOICE=$(echo -e "Copy\nClose" | rofi -mesg "$TRANSLATION" -dmenu -p "Done:")
+    if [ "$CHOICE" = "Copy" ]; then
+      echo -n "$TRANSLATION" | wl-copy
+    elif [ "$CHOICE" = "Close"]; then
+      exit 0
+    elif [ -z "$CHOICE" ]; then
+      exit 0
+    fi
+  done
 fi
